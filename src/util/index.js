f5606c785e3fcb749b13503b3bb576e6c3ff4486
zejia <790868516@qq.com>
2021-07-29 20:16:14 +0800

    feat: add some util

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,18 @@
-export const createNewPetObj = (id, category, name, tags) => {
+export const judgeTeam4Data = (item) => {
+    if (!item || !item.tags) return false
 
+    const tagsNames = item.tags.map(tag => tag.name)
+    if (tagsNames.indexOf("team4") > -1) return true
+    return false
+}
+export const rihtCategory = (category, pet) => {
+    if (category === 'all') return true
+    return pet.category.name === category
+}
+
+export const rightTag = (tag, pet) => {
+    if (tag === 'all') return true
+    const tagsNames = pet.tags.map(tag => tag.name)
+    if (tagsNames.indexOf(tag) > -1) return true
+    return false
 }
