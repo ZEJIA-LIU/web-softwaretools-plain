03062bcfedad323cc9f9ee10c0f97d5271b5ec84
zejia <790868516@qq.com>
2021-07-29 20:15:25 +0800

    feat: get data from api in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,69 @@
 import React from 'react';
 import './App.css'
 import { HashRouter as Router } from "react-router-dom";
+import { Provider } from 'mobx-react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-
+import NewPetStore from './store/newPet'
+import PetArrayStore from './store/petArray'
+import { petArray } from './constant/petData'
+import { findAll, addPet } from './module/index'
 class App extends React.Component {
     constructor(props) {
         super(props)
+        this.newPetStore = new NewPetStore()
+        this.petArrayStore = new PetArrayStore()
+        this.state = {
+            loading: false
+        }
     }
     componentDidMount() {
+        const { add } = this.petArrayStore
+        findAll().then(res => {
+            if (res.length < 15) {
+                this.upLoadData()
+                findAll().then(res => {
+                    for (let i = 0; i < res.length; i++) {
+                        add(res[i])
+                    }
+                })
+                this.setState({ loading: true })
+            } else {
+                for (let i = 0; i < res.length; i++) {
+                    add(res[i])
+                }
+                this.setState({ loading: true })
+            }
+        }).catch(err => {
+            console.log(err)
+        })
 
     }
 
+
+    upLoadData = () => {
+        for (let i = 0; i < petArray.length; i++) {
+            addPet(petArray[i]).then(res => {
+                console.log(`add${petArray[i].name}success: ${res}`)
+            }).catch((err) => {
+                console.log(`add${petArray[i].name}fail: ${err}`)
+            })
+        }
+    }
     render() {
+        const { loading } = this.state
         return (
-            <Router>
-                <Header />
-                <Main />
-                <Footer />
-            </Router>
+
+            loading ? <Provider newPetStore={this.newPetStore} petArrayStore={this.petArrayStore}>
+                <Router>
+                    <Header />
+                    <Main />
+                    <Footer />
+                </Router>
+            </Provider > : <div>loading</div>
+
+
         )
     }
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,33 +3,32 @@ import {
     Switch,
     Route,
 } from "react-router-dom";
-import { Provider } from 'mobx-react'
-import NewPetStore from '../store/newPet'
+
 import Add from '../page/Add'
 import Available from '../page/Available'
 import Sold from '../page/Sold'
 class Component extends React.Component {
     constructor(props) {
         super(props)
-        this.newPetStore = new NewPetStore()
+
     }
     render() {
         return (
-            <Provider newPetStore={this.newPetStore}>
-                <main>
-                    <Switch>
-                        <Route path="/sold">
-                            <Sold />
-                        </Route>
-                        <Route path="/add">
-                            <Add />
-                        </Route>
-                        <Route path="/">
-                            <Available />
-                        </Route>
-                    </Switch>
-                </main>
-            </Provider>
+
+            <main>
+                <Switch>
+                    <Route path="/sold">
+                        <Sold />
+                    </Route>
+                    <Route path="/add">
+                        <Add />
+                    </Route>
+                    <Route path="/">
+                        <Available />
+                    </Route>
+                </Switch>
+            </main>
+
         )
     }
 }
