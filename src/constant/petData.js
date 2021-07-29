fdfe6f897424e4cbe59371b558fcf13eed6e9a55
zejia <790868516@qq.com>
2021-07-29 20:13:27 +0800

    feat: add localData

---
+++ src/constant/petData.js
@@ -0,0 +1,338 @@
+import { categoryMap, categoryArray } from './category'
+import { statusArray } from './status'
+import { tagArray, tagsMap } from './tags'
+import { petName } from './name'
+export const petArray = [
+    {
+        name: petName[0],
+        id: 44001,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] }
+        ]
+    },
+    {
+        name: petName[1],
+        id: 44002,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+    {
+        name: petName[2],
+        id: 44003,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] }
+        ]
+    },
+    {
+        name: petName[3],
+        id: 44004,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[4],
+        id: 44005,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+        ]
+    },
+    {
+        name: petName[5],
+        id: 44006,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+    {
+        name: petName[6],
+        id: 44007,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[7],
+        id: 44008,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+        ]
+    },
+    {
+        name: petName[8],
+        id: 44009,
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+        ]
+    },
+    {
+        name: petName[9],
+        id: 440010,
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+        ]
+    },
+    {
+        name: petName[10],
+        id: 440011,
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[11],
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        id: 440012,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+    {
+        name: petName[12],
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        id: 440013,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+        ]
+    },
+    {
+        name: petName[13],
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        id: 440014,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+        ]
+    },
+    {
+        name: petName[14],
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        id: 440015,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[15],
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        id: 440016,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+    {
+        name: petName[16],
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        id: 440017,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+        ]
+    },
+    {
+        name: petName[17],
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        id: 440018,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+        ]
+    },
+    {
+        name: petName[18],
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        id: 440019,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[19],
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        id: 440020,
+        status: statusArray[0],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+
+    {
+        name: petName[20],
+        id: 440021,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] }
+        ]
+    },
+    {
+        name: petName[21],
+        id: 440022,
+        category: { name: categoryArray[0], id: categoryMap[categoryArray[0]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] }
+        ]
+    },
+    {
+        name: petName[22],
+        id: 440023,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[3], id: tagsMap[tagArray[3]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[23],
+        id: 440024,
+        category: { name: categoryArray[1], id: categoryMap[categoryArray[1]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] }
+        ]
+    },
+    {
+        name: petName[24],
+        id: 440025,
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] }
+        ]
+    },
+    {
+        name: petName[25],
+        id: 440026,
+        category: { name: categoryArray[2], id: categoryMap[categoryArray[2]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] }
+        ]
+    },
+    {
+        name: petName[26],
+        id: 440027,
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[2], id: tagsMap[tagArray[2]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] }
+        ]
+    },
+    {
+        name: petName[27],
+        id: 440028,
+        category: { name: categoryArray[3], id: categoryMap[categoryArray[3]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] }
+        ]
+    },
+    {
+        name: petName[28],
+        id: 440029,
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[1], id: tagsMap[tagArray[1]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] }
+        ]
+    },
+    {
+        name: petName[29],
+        id: 440030,
+        category: { name: categoryArray[4], id: categoryMap[categoryArray[4]] },
+        status: statusArray[1],
+        tags: [
+            { name: tagArray[0], id: tagsMap[tagArray[0]] },
+            { name: tagArray[5], id: tagsMap[tagArray[5]] },
+            { name: tagArray[4], id: tagsMap[tagArray[4]] }
+        ]
+    },
+
+]
