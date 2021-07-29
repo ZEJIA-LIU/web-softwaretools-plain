2b749d3866811802eaf8d14e280ab11717608343
zejia <790868516@qq.com>
2021-07-29 20:16:56 +0800

    feat: finish the search feature

--- src/page/Available.jsx
+++ src/page/Available.jsx
@@ -1,17 +1,111 @@
 import React from 'react'
-import { findByStatus } from '../module/index'
+import { observer, inject } from 'mobx-react'
+import {
+    Form, Button,
+    Select
+} from 'antd';
+import { rightTag, rihtCategory } from '../util/index'
+@inject('petArrayStore')
+@observer
 class Component extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            category: 'all',
+            tag: 'all'
+        }
+    }
+
+    componentDidMount() {
+        const { petArrayStore } = this.props
+        console.log(petArrayStore.curArray)
+    }
+    onFinish = (values) => {
+        console.log(values)
+        const { category, tag } = values
+        this.setState({ category, tag })
     }
     render() {
-        findByStatus("available")
-            .then(res => {
-                const test = res.filter(item => item.category && item.category.name === "dog")
-                console.log(test)
-            })
+        const { Option } = Select
+        const { petArrayStore } = this.props
+        console.log(petArrayStore.curArray)
+        const { category, tag } = this.state
+        const array = petArrayStore.curArray.filter(pet => pet.status === "available").filter((item) => rightTag(tag, item) === true).filter(item => rihtCategory(category, item))
+        console.log(array)
         return (
-            <div>ava</div>
+            <>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 12 }}
+                    initialValues={{ remember: true }}
+                    onFinish={this.onFinish}
+                    onFinishFailed={this.onFinishFailed}
+                >
+                    <Form.Item
+                        name="category"
+                        label="category"
+                        hasFeedback
+                        rules={[{ required: true, message: 'Please select pet\' s category!' }]}
+                        initialValue="all"
+                    >
+                        <Select placeholder="Please select pet' s category!">
+                            <Option value="all">All</Option>
+                            <Option value="cat">Cat</Option>
+                            <Option value="dog">Dog</Option>
+                            <Option value="rabbit">Rabbit</Option>
+                            <Option value="bird">Bird</Option>
+                            <Option value="hamsters">Hamsters</Option>
+                        </Select>
+                    </Form.Item>
+
+
+                    <Form.Item
+                        name="tag"
+                        label="tag"
+                        hasFeedback
+                        initialValue="all"
+                        rules={[{ required: true, message: 'Please select pet\' s tag!' }]}
+                    >
+                        <Select placeholder="Please select pet' s status!">
+                            <Option value="all">All</Option>
+                            <Option value="brave">Brave</Option>
+                            <Option value="mild">Mild</Option>
+                            <Option value="active">Active</Option>
+                            <Option value="lazy">Lzay</Option>
+                            <Option value="elegance">Elegance</Option>
+                        </Select>
+                    </Form.Item>
+
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="add">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <ul>
+                    {array.map((item) => {
+                        return (
+                            <li key={item.id}>
+                                <div>
+                                    <div> name:  {item.name}</div>
+                                    <div>id:{item.id}</div>
+                                    <div>category:{item.category.name}</div>
+                                    <div> tags:{item.tags.map(
+                                        item => {
+                                            if (item.name !== 'team4') {
+                                                return <div>{item.name}</div>
+                                            }
+                                        }
+                                    )}</div>
+
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </>
         )
     }
 }
