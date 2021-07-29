edffb76682eafd9d121713969d8f9e6a2df83f03
zejia <790868516@qq.com>
2021-07-29 20:17:22 +0800

    feat: finish count feature

--- src/page/Sold.jsx
+++ src/page/Sold.jsx
@@ -1,11 +1,69 @@
 import React from 'react'
+import { observer, inject } from 'mobx-react'
+
+
+@inject('petArrayStore')
+@observer
 class Component extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            category: 'all',
+            tags: 'all'
+        }
     }
     render() {
+        const { petArrayStore } = this.props
+        const array = petArrayStore.curArray.filter(pet => pet.status !== "available")
+        const dogNum = petArrayStore.curArray.filter(pet => pet.status !== "available" && pet.category.name === 'dog').length
+        const catNum = petArrayStore.curArray.filter(pet => pet.status !== "available" && pet.category.name === 'cat').length
+        const birdNum = petArrayStore.curArray.filter(pet => pet.status !== "available" && pet.category.name === 'bird').length
+        const rabbitNum = petArrayStore.curArray.filter(pet => pet.status !== "available" && pet.category.name === 'rabbit').length
+        const hamstersNum = petArrayStore.curArray.filter(pet => pet.status !== "available" && pet.category.name === 'hamsters').length
         return (
-            <div>solid</div>
+            <>
+                <ul>
+                    <li>
+                        dogNum : {dogNum}
+                    </li>
+                    <li>
+                        catNum : {catNum}
+                    </li>
+                    <li>
+                        birdNum : {birdNum}
+                    </li>
+                    <li>
+                        rabbitNum : {rabbitNum}
+                    </li>
+                    <li>
+                        hamstersNum : {hamstersNum}
+                    </li>
+                </ul>
+                <ul>
+                    {array.map((item) => {
+                        return (
+                            <li key={item.id}>
+                                <div>
+                                    <div> name:  {item.name}</div>
+                                    <div>id:{item.id}</div>
+                                    <div>category:{item.category.name}</div>
+                                    <div> tags:{item.tags.map(
+                                        item => {
+                                            if (item.name !== 'team4') {
+                                                return <div>{item.name}</div>
+                                            }
+                                        }
+
+                                    )}</div>
+
+
+
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </>
         )
     }
 }
