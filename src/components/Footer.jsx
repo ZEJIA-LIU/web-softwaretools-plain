4123e5388f2d97a3424d7a0d932f86b2d842f8ca
zejia <790868516@qq.com>
2021-08-05 03:43:33 +0800

    feat: adapt style of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,19 +11,33 @@ class Component extends React.Component {
                 <div className='contact'>Contact us</div>
                 <div className='detial'>
                     <div className='item'>
-                        <Icon name='phone' _className='footIcon' />
+                        <div className='footerIconWrapper'>
+                            <Icon name='phone' _className='footIcon' />
+                        </div>
+
                         <div>xxx-xxxxxxxxx </div>
                     </div>
                     <div className='item'>
-                        <Icon name='email' _className='footIcon' />
+                        <div className='footerIconWrapper'>
+                            <Icon name='email' _className='footIcon' />
+                        </div>
                         <div>xxxxxxxxx@xx.com </div>
                     </div>
                     <div className='item'>
-                        <Icon name='adress' _className='footIcon2' />
+                        <div className='footerIconWrapper'>
+                            <Icon name='adress' _className='footIcon' />
+                        </div>
                         <div>No.xx at xxxStreet </div>
                     </div>
 
                 </div>
+                <div className='copyRightWrapper'>
+                    <div className='copyRightIconWrapper'>
+                        <Icon name='copyRight' _className='copyRightIcon' />
+                    </div>
+                    <div className='copyText'>team4.petStore</div>
+                </div>
+
             </footer>
         )
     }
